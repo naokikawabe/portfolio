1d9e49d330c29272b628ea7c1a417c343ac6a35c
naokikawabe <625ruben.bad@gmail.com>
2022-05-04 13:35:09 +0000

    model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :recruiters, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :candidates, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :admins, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
